e5ee121f8232c47075cf80bef6e3b2ac0456b514
Andrei Shevel <ash.developer@yandex.ru>
2016-01-20 15:40:29 +0300

    simple pagination

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 'use strict';
 
 var jade = require('jade'),
+    _ = require('lodash'),
     wld;
 
 module.exports = function (app, logger) {
 
     wld = new (require('./lib/wld'))(logger);
 
-    app.get('/logs', function(req, res, next) {
+    app.get('/logs', function(req, res) {
         res.redirect('/logs/1');
     });
 
-    app.get('/logs/:page', function(req, res, next) {
-        var page = req.params.page || 1;
+    app.get('/logs/:page', function(req, res) {
+        var page = req.params.page || 1,
+            itemsOnPage = 30;
 
         wld.list({
-            limit: 50,
-            offset: (page - 1) * 50
+            limit: itemsOnPage,
+            offset: (page - 1) * itemsOnPage
         }).then(function (logs) {
-            res.send(jade.renderFile(__dirname + '/views/logs.jade', {logs: logs}, null));
+            var pagesCount = Math.floor(wld.count / itemsOnPage) + (wld.count % itemsOnPage === 0 ? 0 : 1);
+
+            res.send(jade.renderFile(__dirname + '/views/logs.jade', {
+                logs: logs,
+                pages: _.range(1, pagesCount + 1)
+            }, null));
         });
 
     });
