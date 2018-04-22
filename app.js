3ecfcf1136b9e4b2bbf2e170fe1600d7eeada826
Andrei Shevel <andrei.shevel@tispr.com>
2018-04-22 18:44:48 +0300

    new version

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
 'use strict';
 
-var jade = require('jade'),
-    _ = require('lodash'),
-    wld;
+const jade = require('jade');
+const _ = require('lodash');
+let wld;
 
 module.exports = function (app, logger) {
+  wld = new (require('./lib/wld'))(logger);
 
-    wld = new (require('./lib/wld'))(logger);
+  app.get('/logs', (req, res) => {
+    res.redirect('/logs/1');
+  });
 
-    app.get('/logs', function(req, res) {
-        res.redirect('/logs/1');
+  app.get('/logs/:page', (req, res) => {
+    const itemsOnPage = 30;
+    let page = _.get(req, 'params.page', 1);
+
+    try {
+      page = parseInt(page);
+    } catch (e) {
+      page = 1;
+    }
+
+    if (page < 1) {
+      page = 1;
+    }
+
+    wld.list({
+      limit: itemsOnPage,
+      offset: (page - 1) * itemsOnPage
+    }).then(function (logs) {
+      res.send(jade.renderFile(__dirname + '/views/logs.jade', {
+        logs,
+        prevPage: page > 1 ? page - 1 : null,
+        nextPage: page + 1
+      }, null));
     });
-
-    app.get('/logs/:page', function(req, res) {
-        var page = req.params.page || 1,
-            itemsOnPage = 30;
-
-        wld.list({
-            limit: itemsOnPage,
-            offset: (page - 1) * itemsOnPage
-        }).then(function (logs) {
-            var pagesCount = Math.floor(wld.count / itemsOnPage) + (wld.count % itemsOnPage === 0 ? 0 : 1);
-
-            res.send(jade.renderFile(__dirname + '/views/logs.jade', {
-                logs: logs,
-                pages: _.range(1, pagesCount + 1)
-            }, null));
-        });
-
-    });
-
+  });
 };
